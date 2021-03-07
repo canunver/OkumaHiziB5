3c05acb0b3c1c00e5023588bf5a5a139ce8b301c
canunver <unvercan@gmail.com>
2021-03-07 21:43:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import OkumaHiziUyg from "./Bilesen/OkumaHiziUyg";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <OkumaHiziUyg />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
